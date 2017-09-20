ad687c6bd30e2eaba0b2fcb16a75088968d754d8
jydozki <ijdm43@gmail.com>
2017-09-20 17:37:13 +0200

    aoc

---
+++ Tutorial.java
@@ -0,0 +1,15 @@
+
+public class Tutorial {
+
+	public static void main(String[] args) {
+		final double PI= 3.142; // Declare a constant
+		//Assign a radius 
+    double radius= 20;
+    //Compute Area
+    double area= radius * radius * PI;
+    //Display Area
+    System.out.println("Area of circle is......." + area);
+    
+	}
+
+}
