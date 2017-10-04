5e8b29080307e7141e3b953141c44f2ff88fdd1d
jydozki <ijdm43@gmail.com>
2017-10-04 21:59:59 +0200

    change

--- Tutorial.java
+++ Tutorial.java
@@ -4,7 +4,7 @@ public class Tutorial {
 	public static void main(String[] args) {
 		final double PI= 3.142; // Declare a constant
 		//Assign a radius 
-    double radius= 20;
+    double radius= 54;
     //Compute Area
     double area= radius * radius * PI;
     //Display Area
