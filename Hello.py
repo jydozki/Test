122a919688c2a8f20eee191822c50959658a2345
jydozki <ijdm43@gmail.com>
2018-03-17 01:07:01 +0100

    pytho ex

---
+++ Hello.py
@@ -0,0 +1,41 @@
+
+def hello():
+	print("Hello")
+
+myName = input("What is your name?")
+print(myName)
+myGender = input("What is your gender?")
+print (myGender)
+Address= input("enter address:")
+print(Address)
+if(myName == "Manuel"):
+   print("Manuel is a boy")
+   
+else:
+   print("he is a girl")
+if(myName == "Manuel"):
+   print("Manuel is a boy!")
+   
+else:
+   print("Manuel is a girl")
+myAge= input("enter age: ")
+print(myAge)
+
+if(myAge >= "20"):
+   print("proceed")
+     
+
+   
+   
+else:
+   print("not eligible")
+   
+   
+   
+hello()
+   
+
+   
+
+   
+   
